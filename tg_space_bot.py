156f2ab08bf7152d7f5d574237d4164fcb641c97
AydarGataullin7 <aydarcr7@gmail.com>
2026-01-06 00:23:35 +0500

    publish photo to Telegram channel

--- tg_space_bot.py
+++ tg_space_bot.py
@@ -6,5 +6,5 @@ load_dotenv()
 TG_TOKEN = os.getenv("TG_TOKEN")
 bot = telegram.Bot(token=TG_TOKEN)
 updates = bot.get_updates()
-bot.send_message(
-    chat_id='@all_ab_sp', text="Text for test😉")
+bot.send_photo(
+    chat_id='@all_ab_sp', photo=open('nasa_images/nasa_5.jpg', 'rb'))
