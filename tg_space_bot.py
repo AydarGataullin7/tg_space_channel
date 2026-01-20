bec778418448a91f27154a6cf50a4357657fbb6c
AydarGataullin7 <aydarcr7@gmail.com>
2026-01-20 23:24:55 +0500

    fix tg_space_bot.py

--- tg_space_bot.py
+++ tg_space_bot.py
@@ -7,9 +7,12 @@ def main():
     load_dotenv()
     tg_token = os.getenv("TG_TOKEN")
     chat_id = os.getenv("TG_CHAT_ID")
+    photo_path = os.getenv("TG_PHOTO_PATH", "nasa_images/nasa_5.jpg")
+
     bot = telegram.Bot(token=tg_token)
     bot.get_updates()
-    with open('nasa_images/nasa_5.jpg', 'rb') as photo_file:
+
+    with open(photo_path, 'rb') as photo_file:
         bot.send_photo(
             chat_id=chat_id,
             photo=photo_file
